86cd9e3f357cb0a1c0c1ecf9ad7d40b7f1aeaaff
cuongyd196 <cuongyd196@gmail.com>
2022-07-09 17:13:11 +0700

    #Update config db

--- server/config/db.js
+++ server/config/db.js
@@ -3,4 +3,10 @@ import { getConfig } from './config';
 
 const config = getConfig(process.env.NODE_ENV);
 mongoose.Promise = global.Promise;
-export const connect = () => mongoose.connect(config.MONGO_URI);
+export const connect = () => mongoose.connect(config.MONGO_URI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true})
+  .catch(err => {
+    setTimeout(connect, 5000);
+  });
